83d464c7f67e2be2f71a8b6d90200ab61304738e
Phil Kruchten <philkruchten@Phils-MacBook-Pro.local>
2026-01-19 14:34:56 -0600

    Fix Layout imports

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,11 @@
-import Header from './Header'
-import Footer from './Footer'
+import {Header} from './Header'
+import {Footer} from './Footer'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
-export default function Layout({children}: LayoutProps) {
+export function Layout({children}: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
